17c539b5ee16381990d5be55032f97218ae39ff1
Kenneth Catungal <kcatungal23101483@student.edu.ph>
2026-04-16 21:07:55 +0800

    Hardcode backend URL to fix login issue

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://thefolio-api-u4i3.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
